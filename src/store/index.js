5c8cc7010e4831cbe79dbceb1556a6c0a206993b
Himanshu-coder007 <himanshunagose54@gmail.com>
2025-05-14 00:00:29 +0530

    redux store peristed

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import videoReducer from "./videoSlice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["videos"], // only videos reducer will be persisted
+};
+
+const persistedReducer = persistReducer(persistConfig, videoReducer);
 
 export const store = configureStore({
   reducer: {
-    videos: videoReducer,
+    videos: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
